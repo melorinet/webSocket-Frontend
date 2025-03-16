a14e5d098707547788ad78ef7d547a3e5230a1f5
Farzad Naghshin <naghshin.farzad@gmail.com>
2025-03-16 00:53:48 -0700

    ws

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,28 @@
 // import React from 'react'
 
-import { Link } from "react-router-dom"
+const WebSocket = require('ws')
+
+const socket = new WebSocket("ws://127.0.0.1:8080/machine")
+
+socket.on("open",()=>{
+  console.log("connected to websocket server")
+  socket.send(JSON.stringify({destination:"/start-machine"}));
+})
+
+socket.on("message",(data)=>{
+  console.log("recived",data)
+})
+
+
+socket.on("close",()=>{
+  console.log("closed")
+})
+
+socket.on("error",(error)=>{
+  console.log("error",error)
+})
+
+import { data, Link } from "react-router-dom"
 import TitleLine from "./components/TitleLine"
 import RightSide from "./components/RightSide"
 import LeftSide from "./components/LeftSide"
